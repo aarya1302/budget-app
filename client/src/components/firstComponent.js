8c637d8f2d5bb3dee96099f25cb69738a57258a2
Aarya Bhorra <1302ab11@gmail.com>
2024-03-08 21:22:43 +0800

    adding routes and pages to the app

--- client/src/components/firstComponent.js
+++ client/src/components/firstComponent.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function MyComponent() {
   const [data, setData] = useState(null);
@@ -7,27 +7,18 @@ function MyComponent() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('/api/data'); // Replace '/api/data' with your actual backend endpoint
+        const response = await axios.get("/api/data"); // Replace '/api/data' with your actual backend endpoint
 
         setData(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
   }, []); // Empty dependency array means this effect runs once after initial render
 
-  return (
-    <div>
-      {data ? (
-        <ul>
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+  return <div>{data ? <ul></ul> : <p>Loading...</p>}</div>;
 }
 
 export default MyComponent;
--- server.js
+++ server.js
@@ -1,66 +1,56 @@
-const express = require('express');
+const express = require("express");
 const axios = require("axios");
-const path = require('path');
-require('dotenv').config();
+const path = require("path");
+require("dotenv").config();
 const app = express();
-
 
 const apiKey = process.env.API_KEY;
 const PORT = process.env.PORT || 3000;
 
-
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // Route to make API request
-app.get('/ping', async (req, res) => {
-  
-});
+app.get("/ping", async (req, res) => {});
 
-app.get('/api/data', async(req, res) => {
+app.get("/api/data", async (req, res) => {
   // Simulate fetching data from a database
-    const data = [{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }];
-    try {
+  const data = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+  ];
+  try {
     // Access token obtained from request headers or wherever it's stored
 
-
-
-
     // Make GET request to the API
-    const response = await axios.get('https://api.up.com.au/api/v1/util/ping', {
+    const response = await axios.get("https://api.up.com.au/api/v1/util/ping", {
       headers: {
-        Authorization: "Bearer "+apiKey
-      }
+        Authorization: "Bearer " + apiKey,
+      },
     });
-    console.log(response.data)
-        
+    console.log(response.data);
 
     // Send the response from the API to the client
     res.json(response.data);
   } catch (error) {
     // Handle errors
-    console.error('Error:', error.message);
-    res.status(error.response.status || 500).json({ error: 'Error fetching data' });
+    console.error("Error:", error.message);
+    res
+      .status(error.response.status || 500)
+      .json({ error: "Error fetching data" });
   }
-  
 });
-
 
 // Serve the React app in production
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get("/auth", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "auth.html"));
 });
 
-
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
